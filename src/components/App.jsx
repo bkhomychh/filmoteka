6ddcfc69ec2f2a750046ec1501a5b2995c3fec84
Bohdan Khomych <bod.khomych@gmail.com>
2023-03-23 21:40:23 +0200

    add redirect to the Home page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,14 @@
-import 'services/moviesAPI';
-import { Routes, Route } from 'react-router-dom';
-import SharedLayout from './SharedLayout';
-
 import { lazy } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import SharedLayout from 'components/SharedLayout';
 
 const Home = lazy(() => import('pages/Home'));
 const Bookmarks = lazy(() => import('pages/Bookmarks'));
 const Search = lazy(() => import('pages/Search'));
 const MovieDetails = lazy(() => import('pages/MovieDetails'));
 
-export const App = () => {
+const App = () => {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
@@ -17,7 +16,10 @@ export const App = () => {
         <Route path="search" element={<Search />} />
         <Route path="search/:movieId" element={<MovieDetails />} />
         <Route path="bookmarks" element={<Bookmarks />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
   );
 };
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import './assets/sass/index.scss';
-import { App } from 'components/App';
+import App from 'components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
